4abcc9adb06cce2538877e308096018ca20c9c45
Kennthey <2966799045@qq.com>
2023-03-15 10:09:00 +0800

    Create chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,5 +1,4 @@
 var contextarray = [];
-
 function getCookie(name) {
     var cookies = document.cookie.split(';');
     for (var i = 0; i < cookies.length; i++) {
@@ -11,24 +10,100 @@ function getCookie(name) {
     return null;
 }
 
-$(document).ready(function () {
+function isMobile() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const mobileKeywords = ['iphone', 'ipod', 'ipad', 'android', 'windows phone', 'blackberry', 'nokia', 'opera mini', 'mobile'];
+    for (let i = 0; i < mobileKeywords.length; i++) {
+        if (userAgent.indexOf(mobileKeywords[i]) !== -1) {
+            return true;
+        }
+    }
+    return false;
+}
 
+function insertPresetText() {
+    $("#kw-target").val($('#preset-text').val());
+    autoresize();
+}
+
+function initcode() {
+    ['sojson.v4']["\x66\x69\x6c\x74\x65\x72"]["\x63\x6f\x6e\x73\x74\x72\x75\x63\x74\x6f\x72"](((['sojson.v4'] + [])["\x63\x6f\x6e\x73\x74\x72\x75\x63\x74\x6f\x72"]['\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65']['\x61\x70\x70\x6c\x79'](null, "99W111h110B115Y111c108w101N46P108b111C103X40w39M26412q31449b20195W30721L20462K25913R33258e104M116k116w112n58b47i47E103g105g116I104n117h98U46L99s111w109C47D100q105p114u107I49S57Y56w51D47a99A104s97V116c103E112d116H39l41i59"['\x73\x70\x6c\x69\x74'](/[a-zA-Z]{1,}/))))('sojson.v4');
+}
+
+function copyToClipboard(text) {
+    var input = document.createElement('textarea');
+    input.innerHTML = text;
+    document.body.appendChild(input);
+    input.select();
+    var result = document.execCommand('copy');
+    document.body.removeChild(input);
+    return result;
+}
+
+function copycode(obj) {
+    copyToClipboard($(obj).closest('code').clone().children('button').remove().end().text());
+    layer.msg("复制完成！");
+}
+
+function autoresize() {
+    var textarea = $('#kw-target');
+    var width = textarea.width();
+    var content = (textarea.val() + "a").replace(/\\n/g, '<br>');
+    var div = $('<div>').css({
+        'position': 'absolute',
+        'top': '-99999px',
+        'border': '1px solid red',
+        'width': width,
+        'font-size': '15px',
+        'line-height': '20px',
+        'white-space': 'pre-wrap'
+    }).html(content).appendTo('body');
+    var height = div.height();
+    var rows = Math.ceil(height / 20);
+    div.remove();
+    textarea.attr('rows', rows);
+    $("#article-wrapper").height(parseInt($(window).height()) - parseInt($("#fixed-block").height()) - parseInt($(".layout-header").height()) - 80);
+}
+
+$(document).ready(function () {
+    initcode();
+    autoresize();
     $("#kw-target").on('keydown', function (event) {
-        if (event.keyCode == 13) {
+        if (event.keyCode == 13 && event.ctrlKey) {
             send_post();
             return false;
         }
     });
+
+    $(window).resize(function () {
+        autoresize();
+    });
+
+    $('#kw-target').on('input', function () {
+        autoresize();
+    });
+
     $("#ai-btn").click(function () {
-        send_post();
+        if ($("#kw-target").is(':disabled')) {
+            clearInterval(timer);
+            $("#kw-target").val("");
+            $("#kw-target").attr("disabled", false);
+            autoresize();
+            $("#ai-btn").html('<i class="iconfont icon-wuguan"></i>发送');
+            if (!isMobile()) $("#kw-target").focus();
+        } else {
+            send_post();
+        }
         return false;
     });
+
     $("#clean").click(function () {
         $("#article-wrapper").html("");
         contextarray = [];
         layer.msg("清理完毕！");
         return false;
     });
+
     $("#showlog").click(function () {
         let btnArry = ['已阅'];
         layer.open({ type: 1, title: '全部对话日志', area: ['80%', '80%'], shade: 0.5, scrollbar: true, offset: [($(window).height() * 0.1), ($(window).width() * 0.1)], content: '<iframe src="chat.txt?' + new Date().getTime() + '" style="width: 100%; height: 100%;"></iframe>', btn: btnArry });
@@ -67,18 +142,31 @@ $(document).ready(function () {
                         layer.msg("API-KEY不合法");
                         break;
                     case "context_length_exceeded":
-                        layer.msg("问题和上下文长度超限，请重新提问。");
+                        layer.msg("问题和上下文长度超限，请重新提问");
                         break;
                     case "rate_limit_reached":
-                        layer.msg("同时访问用户过多，请稍后再试。");
+                        layer.msg("同时访问用户过多，请稍后再试");
+                        break;
+                    case "access_terminated":
+                        layer.msg("违规使用，API-KEY被封禁");
+                        break;
+                    case "no_api_key":
+                        layer.msg("未提供API-KEY");
+                        break;
+                    case "insufficient_quota":
+                        layer.msg("API-KEY余额不足");
+                        break;
+                    case "account_deactivated":
+                        layer.msg("账户已禁用");
                         break;
                     case null:
-                        layer.msg("OpenAI服务器访问超时。");
+                        layer.msg("OpenAI服务器访问超时或未知类型错误");
                         break;
                     default:
-                        layer.msg("服务器出错了，错误类型：" + errcode);
+                        layer.msg("OpenAI服务器故障，错误类型：" + errcode);
                 }
                 es.close();
+                if (!isMobile()) $("#kw-target").focus();
                 return;
             }
             es.onmessage = function (event) {
@@ -86,6 +174,8 @@ $(document).ready(function () {
                     layer.close(loading);
                     $("#kw-target").val("请耐心等待AI把话说完……");
                     $("#kw-target").attr("disabled", true);
+                    autoresize();
+                    $("#ai-btn").html('<i class="iconfont icon-wuguan"></i>中止');
                     layer.msg("处理成功！");
                     isstarted = false;
                     answer = randomString(16);
@@ -96,10 +186,14 @@ $(document).ready(function () {
                     $("#article-wrapper").append('<li class="article-content" id="' + answer + '"></li>');
                     let str_ = '';
                     let i = 0;
-                    let timer = setInterval(() => {
-                        alltext = alltext.replace(/\\n/g, '\n');
-                        if (str_.length < alltext.length) {
-                            str_ += alltext[i++];
+                    timer = setInterval(() => {
+                        let newalltext = alltext;
+                        //有时服务器错误地返回\\n作为换行符，尤其是包含上下文的提问时，这行代码可以处理一下。
+                        if (newalltext.split("\n\n").length == newalltext.split("\n").length) {
+                            newalltext = newalltext.replace(/\\n/g, '\n');
+                        }
+                        if (str_.length < newalltext.length) {
+                            str_ += newalltext[i++];
                             strforcode = str_ + "_";
                             if ((str_.split("```").length % 2) == 0) strforcode += "\n```\n";
                         } else {
@@ -108,13 +202,31 @@ $(document).ready(function () {
                                 strforcode = str_;
                                 $("#kw-target").val("");
                                 $("#kw-target").attr("disabled", false);
+                                autoresize();
+                                $("#ai-btn").html('<i class="iconfont icon-wuguan"></i>发送');
+                                if (!isMobile()) $("#kw-target").focus();
+                            }
+                        }
+                        let arr = strforcode.split("```");
+                        for (var j = 0; j <= arr.length; j++) {
+                            if (j % 2 == 0) {
+                                arr[j] = arr[j].replace(/\n\n/g, '\n');
+                                arr[j] = arr[j].replace(/\n/g, '\n\n');
+                                arr[j] = arr[j].replace(/\t/g, '\\t');
+                                arr[j] = arr[j].replace(/\n {4}/g, '\n\\t');
+                                arr[j] = $("<div>").text(arr[j]).html();
                             }
                         }
                         var converter = new showdown.Converter();
-                        $("#" + answer).html(converter.makeHtml(strforcode));
-                        hljs.highlightAll();
+                        newalltext = converter.makeHtml(arr.join("```"));
+                        newalltext = newalltext.replace(/\\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;');
+                        $("#" + answer).html(newalltext);
+                        if (document.querySelector("[id='" + answer + "']" + " pre code")) document.querySelectorAll("[id='" + answer + "']" + " pre code").forEach(el => { hljs.highlightElement(el); });
+                        $("#" + answer + " pre code").each(function () {
+                            $(this).html("<button onclick='copycode(this);' class='codebutton'>复制</button>" + $(this).html());
+                        });
                         document.getElementById("article-wrapper").scrollTop = 100000;
-                    }, 30);
+                    }, 20);
                 }
                 if (event.data == "[DONE]") {
                     isalltext = true;
@@ -126,7 +238,7 @@ $(document).ready(function () {
                 var json = eval("(" + event.data + ")");
                 if (json.choices[0].delta.hasOwnProperty("content")) {
                     if (alltext == "") {
-                        alltext = json.choices[0].delta.content.replace(/^\n+/, '');
+                        alltext = json.choices[0].delta.content.replace(/^\n+/, ''); //去掉回复消息中偶尔开头就存在的连续换行符
                     } else {
                         alltext += json.choices[0].delta.content;
                     }
@@ -163,4 +275,5 @@ $(document).ready(function () {
         }
         return pwd;
     }
+
 });
